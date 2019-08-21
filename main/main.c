7ea3cd568ac13decdf11021a49d81a857c53dc2e
Jannik Kolodziej <git@jko.dev>
2019-08-21 21:14:32 +0200

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include "test.h"
+
+
+
+void app_main()
+{
+
+}
